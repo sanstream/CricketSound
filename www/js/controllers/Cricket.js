96159a35c3aea9d9808f1d32282583c63cc1d586
Sanne Peters <sanstream@gmail.com>
2015-05-20 08:04:46 +0200

    Integrated Angular into the app.

---
+++ www/js/controllers/Cricket.js
@@ -0,0 +1,23 @@
+/**
+ * @description Holds all the logic for the ChapterCtrl controller.
+ * @author 		Sanne Peters
+ * @copyright 	Sanstream Creations 2013  
+ */
+
+/**
+ * Definition of the ChapterCtrl Angular controller:
+ */
+
+CricketSound.controller('Cricket',['$scope',
+ 	function ($scope){
+
+		$scope.one = 1;
+		/**
+		 * Handles the result of the toggling action of the in-page index button
+		 * @return {void}
+		 */
+		$scope.aMethod = function(){
+
+		}
+	}
+]);
